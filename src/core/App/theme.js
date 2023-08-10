9c6d1f25f328b9f2877581c9f217546c43742e16
Robert Fydrych <123332510+RobFyd@users.noreply.github.com>
2023-08-10 23:59:54 +0100

    common in theme.js added

--- src/core/App/theme.js
+++ src/core/App/theme.js
@@ -16,3 +16,13 @@ const colorNames = {
   mineShaft: "#252525",
   mineShaftLighter: "#313131",
 };
+
+const common = {
+    breakpoints: {
+        mobileMax: 767,
+        tabletyVerticalMax: 991,
+        tabletHorizontalMax: 1199,
+    },
+    boxShadow: "0px -2px 50px rgba(9, 10, 51, 0.02), 0px 16px 58px rgba(9, 10, 51, 0.03)";
+    borderRadiusSmall: "4px",
+};
