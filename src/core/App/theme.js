8230f7ed55b3f915c5b633fa234b4053cf9d9685
Robert Fydrych <123332510+RobFyd@users.noreply.github.com>
2023-08-11 23:53:07 +0100

    themeLight and themeDark colors added

--- src/core/App/theme.js
+++ src/core/App/theme.js
@@ -2,7 +2,7 @@ const colorNames = {
   scienceBlue: "#0366D6",
   blumine: "#1F5189",
   dodgerBlue: "#2188FF",
-  shipCoce: "#6D93BE",
+  shipCove: "#6D93BE",
   anakiwa: "#8CC2FF",
   tropicalBlue: "#CDE0F7",
   ironTransparent: "rgba(209, 213, 218, 0.3)",
@@ -18,11 +18,68 @@ const colorNames = {
 };
 
 const common = {
-    breakpoints: {
-        mobileMax: 767,
-        tabletyVerticalMax: 991,
-        tabletHorizontalMax: 1199,
+  breakpoints: {
+    mobileMax: 767,
+    tabletyVerticalMax: 991,
+    tabletHorizontalMax: 1199,
+  },
+  boxShadow:
+    "0px -2px 50px rgba(9, 10, 51, 0.02), 0px 16px 58px rgba(9, 10, 51, 0.03)",
+  borderRadiusSmall: "4px",
+};
+
+export const themeLight = {
+  ...common,
+  colors: {
+    primary: colorNames.scienceBlue,
+    textPrimary: colorNames.mineShaft,
+    site: {
+      background: colorNames.whiteLilac,
+      text: colorNames.slateGray,
     },
-    boxShadow: "0px -2px 50px rgba(9, 10, 51, 0.02), 0px 16px 58px rgba(9, 10, 51, 0.03)";
-    borderRadiusSmall: "4px",
+    buttonLink: {
+      text: colorNames.white,
+      border: colorNames.ironTransparent,
+      shadow: colorNames.anakiwa,
+    },
+    boxBackground: colorNames.white,
+    headreLine: colorNames.mercury,
+    title: {
+      border: colorNames.porcelain,
+      borderHover: colorNames.tropicalBlue,
+      header: colorNames.scienceBlue,
+    },
+    themeSwitch: {
+      background: colorNames.mercury,
+      icon: colorNames.white,
+    },
+  },
+};
+
+export const themeDark = {
+  ...common,
+  colors: {
+    primary: colorNames.dodgerBlue,
+    textPrimary: colorNames.white,
+    site: {
+      background: colorNames.mineShaft,
+      text: colorNames.white,
+    },
+    buttonLink: {
+      text: colorNames.white,
+      border: colorNames.ironTransparent,
+      shadow: colorNames.shipCove,
+    },
+    boxBackground: colorNames.mineShaftLighter,
+    headreLine: colorNames.tundora,
+    title: {
+      border: colorNames.tundora,
+      borderHover: colorNames.blumine,
+      header: colorNames.white,
+    },
+    themeSwitch: {
+      background: colorNames.doveGray,
+      icon: colorNames.mineShaft,
+    },
+  },
 };
