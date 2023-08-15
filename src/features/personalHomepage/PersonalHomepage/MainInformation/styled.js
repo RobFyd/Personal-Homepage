52dd8d5e7b8c4290075a1217ce346600b3bb2a3d
Robert Fydrych <123332510+RobFyd@users.noreply.github.com>
2023-08-16 00:00:25 +0100

    MainInformation styled.js updated

--- src/features/personalHomepage/PersonalHomepage/MainInformation/styled.js
+++ src/features/personalHomepage/PersonalHomepage/MainInformation/styled.js
@@ -1,9 +1,23 @@
 import styled from "styled-components";
 import { ButtonLink } from "../ButtonLink";
+import { themeLight } from "../../../../core/App/theme";
 // import {ReactComponent as EnvelopeIcon} from "./envelope.svg";
 
 export const Wrapper = styled.header`
   margin-top: -30px;
+  display: grid;
+  grid-template-columns: auto 1fr;
+  grid-gap: 64px;
+  align-items: center;
+
+  @media(max-width: ${({theme}) => themeLight.breakpoints.tabletVertivalMax}px) {
+    grid-gap: 32px;
+  }
+
+  @media(max-width: ${({theme}) => themeLight.breakpoints.mobileMax}px) {
+    grid-template-columns: 1fr;
+    grid-gap: 12px;
+  }
 `;
 
 export const Avatar = styled.img`
