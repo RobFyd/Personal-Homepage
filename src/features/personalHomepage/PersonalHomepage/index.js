d1c2ea9e6ee0cc60f5ca5fcae1528ffe0196a01d
Robert Fydrych <123332510+RobFyd@users.noreply.github.com>
2023-08-19 22:55:36 +0100

    Skills added to PersonalHomepage

--- src/features/personalHomepage/PersonalHomepage/index.js
+++ src/features/personalHomepage/PersonalHomepage/index.js
@@ -1,11 +1,12 @@
 import { Container } from "./styled";
 import { MainInformation } from "./MainInformation";
+import { Skills } from "./Skills";
 
 export const PersonalHomepage = () => (
   <Container>
     <MainInformation />
     <main>
-      
+      <Skills />
     </main>
   </Container>
 );
