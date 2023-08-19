0786f250d380e3b611065474e8ee1c54eb004888
Robert Fydrych <123332510+RobFyd@users.noreply.github.com>
2023-08-20 00:11:21 +0100

    SubHeader index.js updated

--- src/features/personalHomepage/PersonalHomepage/SubHeader/index.js
+++ src/features/personalHomepage/PersonalHomepage/SubHeader/index.js
@@ -1,6 +1,13 @@
 import styled from "styled-components";
+import { themeLight } from "../../../../core/App/theme";
 
 export const SubHeader = styled.h2`
   margin: 0;
   font-size: 30px;
+  font-weight: 900;
+  color: ${({ theme }) => themeLight.colors.textPrimary};
+
+  @media (max-width: ${({ theme }) => themeLight.breakpoints.mobileMax}px) {
+    font-size: 18px;
+  }
 `;
