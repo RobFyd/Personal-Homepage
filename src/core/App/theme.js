7ca15dd1e2a485e09e3cb5f38775c87a3f666e82
Robert Fydrych <123332510+RobFyd@users.noreply.github.com>
2023-08-20 00:04:31 +0100

    Skills styled

--- src/core/App/theme.js
+++ src/core/App/theme.js
@@ -23,8 +23,10 @@ const common = {
     tabletVerticalMax: 991,
     tabletHorizontalMax: 1199,
   },
+
   boxShadow:
-    "0px -2px 50px rgba(9, 10, 51, 0.02), 0px 16px 58px rgba(9, 10, 51, 0.03)",
+    "0 -2px 50px rgba(9, 10, 51, 0.02), 0 16px 58px rgba(9, 10, 51, 0.03)",
+
   borderRadiusSmall: "4px",
 };
 
---
+++ src/features/personalHomepage/PersonalHomepage/SubHeader/index.js
@@ -0,0 +1,6 @@
+import styled from "styled-components";
+
+export const SubHeader = styled.h2`
+  margin: 0;
+  font-size: 30px;
+`;
