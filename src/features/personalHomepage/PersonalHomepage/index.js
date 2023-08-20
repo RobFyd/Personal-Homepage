d95c05eb78a89d446d0b202acb86f0032a6d4106
Robert Fydrych <123332510+RobFyd@users.noreply.github.com>
2023-08-20 22:13:15 +0100

    skills icons added and styled

--- src/features/personalHomepage/PersonalHomepage/index.js
+++ src/features/personalHomepage/PersonalHomepage/index.js
@@ -1,14 +1,30 @@
-import { Container } from "./styled";
+import { Container, Icon } from "./styled";
 import { MainInformation } from "./MainInformation";
 import { Skills } from "./Skills";
 import { skills, nextSkills } from "./skillsData";
+import toolsIcon from "./toolsIcon.png";
+import rocketIcon from "./rocketIcon.png";
 
 export const PersonalHomepage = () => (
   <Container>
     <MainInformation />
     <main>
-      <Skills title={<>My skillset includes</>} skills={skills} />
-      <Skills title={<>What I want to learn next</>} skills={nextSkills} />
+      <Skills
+        title={
+          <>
+            My skillset includesc <Icon src={toolsIcon} alt="" />
+          </>
+        }
+        skills={skills}
+      />
+      <Skills
+        title={
+          <>
+            What I want to learn next <Icon src={rocketIcon} alt="" />
+          </>
+        }
+        skills={nextSkills}
+      />
     </main>
   </Container>
 );
--- src/features/personalHomepage/PersonalHomepage/styled.js
+++ src/features/personalHomepage/PersonalHomepage/styled.js
@@ -1,7 +1,21 @@
 import styled from "styled-components";
+import { themeLight } from "../../../core/App/theme";
 
 export const Container = styled.main`
   max-width: 1248px;
-  margin: 0 auto 109px;
   padding: 16px;
+  margin: auto;
+
+  @media (max-width: ${({ theme }) => themeLight.breakpoints.mobileMax}px) {
+    padding: 8px;
+  }
+`;
+
+export const Icon = styled.img`
+  vertical-align: middle;
+  margin-left: 5px;
+
+  @media (max-width: ${({ theme }) => themeLight.breakpoints.mobileMax}px) {
+    height: 16px;
+  }
 `;
