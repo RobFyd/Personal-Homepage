cb73007aa091e553c6a1d6a8ce57ef264617f115
Robert Fydrych <123332510+RobFyd@users.noreply.github.com>
2023-08-20 22:54:15 +0100

    socials.js added

---
+++ src/features/personalHomepage/PersonalHomepage/Footer/SocialIcons/socials.js
@@ -0,0 +1,28 @@
+import { styleIcon } from "./styled";
+import { ReactComponent as GithubIcon } from "./icons/github.svg";
+import { ReactComponent as FacebookIcon } from "./icons/facebook.svg";
+import { ReactComponent as LinkedInIcon } from "./icons/linkedin.svg";
+import { ReactComponent as InstagramIcon } from "./icons/instagram.svg";
+
+export const socials = [
+  {
+    name: "GitHub",
+    url: "https://github.com",
+    Icon: styleIcon(GithubIcon),
+  },
+  {
+    name: "Facebook",
+    url: "https://facebook.com",
+    Icon: styleIcon(FacebookIcon),
+  },
+  {
+    name: "LinkedIn",
+    url: "https://linkedin.com",
+    Icon: styleIcon(LinkedInIcon),
+  },
+  {
+    name: "Instagram",
+    url: "https://instagram.com",
+    Icon: styleIcon(InstagramIcon),
+  },
+];
