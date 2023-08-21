dd5e7ad29a4f6fab9bc5123143139e10a4758620
Robert Fydrych <123332510+RobFyd@users.noreply.github.com>
2023-08-22 00:29:40 +0100

    Footer upgraded

--- src/features/personalHomepage/PersonalHomepage/Footer/index.js
+++ src/features/personalHomepage/PersonalHomepage/Footer/index.js
@@ -1,7 +1,7 @@
 import { email } from "../email";
 import { SocialIcons } from "./SocialIcons";
 import {
-  Adderess,
+  Address,
   EmailLink,
   EmailWrapper,
   LetsTalk,
@@ -9,7 +9,19 @@ import {
   Wrapper,
 } from "./styled";
 
-
 export const Footer = () => (
-<Wrapper>h</Wrapper>
+  <Wrapper>
+    <LetsTalk>Let's talk!</LetsTalk>
+    <Address>
+      <EmailWrapper>
+        <EmailLink href={`mailto:${email}`}>{email}</EmailLink>
+      </EmailWrapper>
+      <Paragraph>
+        I'm always open to new projects whenever I have time. If you have
+        a&nbsp;website, dashboard or a mobile app in mind and need some help
+        to&nbsp;make your ideas come to life, feel free to contact me ✌️
+      </Paragraph>
+      <SocialIcons />
+    </Address>
+  </Wrapper>
 );
