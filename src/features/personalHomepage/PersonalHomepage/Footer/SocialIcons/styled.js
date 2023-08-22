2a965e19c2e8984c704979086b9691bd39abe90f
Robert Fydrych <123332510+RobFyd@users.noreply.github.com>
2023-08-23 00:14:22 +0100

    Icons styled

--- src/features/personalHomepage/PersonalHomepage/Footer/SocialIcons/styled.js
+++ src/features/personalHomepage/PersonalHomepage/Footer/SocialIcons/styled.js
@@ -1,19 +1,37 @@
 import styled from "styled-components";
+import { themeLight } from "../../../../../core/App/theme";
 
 export const List = styled.ul`
+  margin-top: 56px;
+  margin-bottom: 0;
   display: flex;
+  list-style: none;
+  padding: 0;
 `;
 
 export const Item = styled.li`
   &:not(:last-child) {
-    margin-right: 16px;
+    margin-right: 24px;
+
+    @media (max-width: ${({ theme }) => themeLight.breakpoints.mobileMax}px) {
+      margin-right: 16px;
+    }
   }
 `;
 
 export const Link = styled.a`
-  color: black;
+  color: ${({ theme }) => themeLight.colors.textPrimary};
+  transition: color 0.3s;
+
+  &:hover {
+    color: ${({ theme }) => themeLight.colors.primary};
+  }
 `;
 
 export const styleIcon = (Icon) => styled(Icon)`
   height: auto;
+
+  @media (max-width: ${({ theme }) => themeLight.breakpoints.mobileMax}px) {
+    width: 32px;
+  }
 `;
