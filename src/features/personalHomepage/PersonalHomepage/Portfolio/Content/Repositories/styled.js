a96821b88f5daa217b2eddab660dad8688daef6e
Robert Fydrych <123332510+RobFyd@users.noreply.github.com>
2023-08-24 23:55:14 +0100

    Portfolio Repositories styled.js updated

--- src/features/personalHomepage/PersonalHomepage/Portfolio/Content/Repositories/styled.js
+++ src/features/personalHomepage/PersonalHomepage/Portfolio/Content/Repositories/styled.js
@@ -9,36 +9,29 @@ export const List = styled.ul`
   padding: 0;
   grid-gap: 32px;
 
-  @media (max-width: ${({ theme }) => themeLight.breakpoints.tabletVerticalMax}px) {
+  @media (max-width: ${({ theme }) =>
+      themeLight.breakpoints.tabletVerticalMax}px) {
     grid-template-columns: 1fr;
     grid-gap: 24px;
   }
 `;
 
 export const Tile = styled.li`
+  padding: 56px;
+  margin: 0;
+  background: ${({ theme }) => themeLight.colors.boxBackground};
+  border: 6px solid ${({ theme }) => themeLight.colors.title.border};
+  transition: border-color 0.3s;
+`;
 
-`
+export const Name = styled.h3``;
 
-export const Name = styled.h3`
+export const Description = styled.p``;
 
-`
+export const Links = styled.dl``;
 
-export const Description = styled.p`
+export const LinksRow = styled.div``;
 
-`
+export const LinksValue = styled.dd``;
 
-export const Links = styled.dl`
-
-`
-
-export const LinksRow = styled.div`
-
-`
-
-export const LinksValue = styled.dd`
-
-`
-
-export const Link = styled.a`
-
-`
+export const Link = styled.a``;
