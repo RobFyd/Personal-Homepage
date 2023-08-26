5199855887cd90a1e0fb965fe51142b47b915545
Robert Fydrych <123332510+RobFyd@users.noreply.github.com>
2023-08-26 21:21:27 +0100

    themeSlice added, ThemeSwitch index.js upgraded

--- src/common/ThemeSwitch/index.js
+++ src/common/ThemeSwitch/index.js
@@ -1,17 +1,17 @@
-// import { useDispatch, useSelector } from "react-redux";
-// import { selectIsDarkTheme, toggleTheme } from "../themeSlice";
+import { useDispatch, useSelector } from "react-redux";
+import { selectIsDarkTheme, toggleTheme } from "../themeSlice";
 import { Box, Button, Icon, IconWrapper, Text, Wrapper } from "./styled";
 
 export const ThemeSwitch = () => {
-  // const isDarkTheme = useSelector(selectIsDarkTheme);
-  // const dispatch = useDispatch();
+  const isDarkTheme = useSelector(selectIsDarkTheme);
+  const dispatch = useDispatch();
 
   return (
     <Wrapper>
-      <Button>
-        <Text>Dark mode</Text>
+      <Button onClick={() => dispatch(toggleTheme())}>
+        <Text>Dark mode {isDarkTheme ? "on" : "off"}</Text>
         <Box>
-          <IconWrapper>
+          <IconWrapper moveToRight={isDarkTheme}>
             <Icon />
           </IconWrapper>
         </Box>
@@ -19,21 +19,3 @@ export const ThemeSwitch = () => {
     </Wrapper>
   );
 };
-
-// export const ThemeSwitch = () => {
-//   const isDarkTheme = useSelector(selectIsDarkTheme);
-//   const dispatch = useDispatch();
-
-//   return (
-//     <Wrapper>
-//       <Button onClick={() => dispatch(toggleTheme())}>
-//         <Text>Dark mode {isDarkTheme ? "on" : "off"}</Text>
-//         <Box>
-//           <IconWrapper moveToRight={isDarkTheme}>
-//             <Icon />
-//           </IconWrapper>
-//         </Box>
-//       </Button>
-//     </Wrapper>
-//   );
-// };
