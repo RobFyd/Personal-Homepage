602e8987a9eb21635e3b49c50febf249ef380929
Robert Fydrych <123332510+RobFyd@users.noreply.github.com>
2023-08-27 23:35:43 +0100

    Loading, Error added

--- src/features/personalHomepage/PersonalHomepage/Portfolio/Content/index.js
+++ src/features/personalHomepage/PersonalHomepage/Portfolio/Content/index.js
@@ -1,15 +1,24 @@
 import { Repositories } from "./Repositories";
-// import {Loading} from "./Loading";
-// import {Error} from "./Error";
+import { Loading } from "./Loading";
+import { Error } from "./ErrorBox";
 
 const Content = ({ status, repositories }) => {
+  return <Error />;
   switch (status) {
     case "initial":
       return null;
 
+    case "loading":
+      return <Loading />;
+
+    case "error":
+      return <Error />;
+
     case "success":
       return <Repositories repositories={repositories} />;
-      
+
+    default:
+      throw new Error(`Incorrect status: ${status}`);
   }
 };
 
