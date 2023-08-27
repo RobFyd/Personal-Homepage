bb271ced8b0a5c8bbbdf14ad16856a776e43c3d1
Robert Fydrych <123332510+RobFyd@users.noreply.github.com>
2023-08-27 22:09:45 +0100

    personalHomePageSaga added

--- src/features/personalHomepage/personalHomePageSaga.js
+++ src/features/personalHomepage/personalHomePageSaga.js
@@ -1,3 +1,18 @@
-import { takeLatest, call, put, delayed } from "redux-saga/effects";
-import {fetchRepositories, fetchRepositoriesSuccess, fetchRepositoriesError} from "./personalHomePageSlice";
-import { getRepositories} from "./personalHomePageApi";
+import { takeLatest, call, put, delay } from "redux-saga/effects";
+import {
+  fetchRepositories,
+  fetchRepositoriesSuccess,
+} from "./personalHomePageSlice"; //fetchRepositoriesError
+import { getRepositories } from "./personalHomePageApi";
+
+const loadingDelay = 2_000;
+
+function* fetchRepositoriesHandler({ payload: username }) {
+  yield delay(loadingDelay);
+  const repositories = yield call(getRepositories, username);
+  yield put(fetchRepositoriesSuccess(repositories));
+}
+
+export function* personalHomepageSaga() {
+  yield takeLatest(fetchRepositories.type, fetchRepositoriesHandler);
+}
