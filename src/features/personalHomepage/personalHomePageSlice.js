66f26a4a868229e65b4ef39f069d8bb636d22a01
Robert Fydrych <123332510+RobFyd@users.noreply.github.com>
2023-08-27 22:10:17 +0100

    personalHomePageSlice completed

--- src/features/personalHomepage/personalHomePageSlice.js
+++ src/features/personalHomepage/personalHomePageSlice.js
@@ -1,1 +1,31 @@
 import { createSlice } from "@reduxjs/toolkit";
+
+const personalHomepageSlice = createSlice({
+  name: "personalHomepage",
+  initialState: {
+    repositories: null,
+    status: "initial",
+  },
+  reducers: {
+    fetchRepositories: () => ({
+      status: "loading",
+      repositories: null,
+    }),
+    fetchRepositoriesSuccess: (_, { payload: repositories }) => ({
+      status: "success",
+      repositories,
+    }),
+  },
+});
+
+export const { fetchRepositories, fetchRepositoriesSuccess } = personalHomepageSlice.actions;
+
+const selectPersonalHomepageState = (state) => state.personalHomepage;
+
+export const selectRepositories = (state) =>
+  selectPersonalHomepageState(state).repositories;
+
+export const selectRepositoriesStatus = (state) =>
+  selectPersonalHomepageState(state).status;
+
+export default personalHomepageSlice.reducer;
