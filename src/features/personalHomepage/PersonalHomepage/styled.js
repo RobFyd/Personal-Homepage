4b90cb659abd4087b6a4b79b7f76764cd51a8ac2
Robert Fydrych <123332510+RobFyd@users.noreply.github.com>
2024-09-07 21:36:36 +0100

    Icon, Icon2 animated

--- src/features/personalHomepage/PersonalHomepage/styled.js
+++ src/features/personalHomepage/PersonalHomepage/styled.js
@@ -11,6 +11,57 @@ export const Container = styled.main`
 `;
 
 export const Icon = styled.img`
+  @keyframes shake {
+    0%,
+    5%,
+    10%,
+    15%,
+    20%,
+    80%,
+    85%,
+    90%,
+    95%,
+    100% {
+      transform: rotate(0deg) translateX(0);
+    }
+    25% {
+      transform: rotate(-8deg) translateX(-4px);
+    }
+    50% {
+      transform: rotate(8deg) translateX(4px);
+    }
+    75% {
+      transform: rotate(-8deg) translateX(-4px);
+    }
+  }
+  animation: shake 0.8s ease infinite;
+  vertical-align: middle;
+  margin-left: 5px;
+
+  @media (max-width: ${({ theme }) => theme.breakpoints.mobileMax}px) {
+    height: 24px;
+  }
+`;
+
+export const Icon2 = styled.img`
+  @keyframes rocket {
+    0% {
+      transform: translate(0, 0) rotate(0deg);
+    }
+    25% {
+      transform: translate(-1px, 3px) rotate(-1deg);
+    }
+    50% {
+      transform: translate(0, 0) rotate(0deg);
+    }
+    75% {
+      transform: translate(1px, -3px) rotate(1deg);
+    }
+    100% {
+      transform: translate(0, 0) rotate(0deg);
+    }
+  }
+  animation: rocket 1.5s ease-in-out infinite;
   vertical-align: middle;
   margin-left: 5px;
 
